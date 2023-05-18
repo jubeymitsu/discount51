ff04356abc69f16a9fc21e71045dfa4915e8ca22
jubeymitsu <jubey.mitsu@gmail.com>
2023-05-19 01:49:49 +0300

    Start again

---
+++ src/main/java/ru/stomprf/discount51/Discount51Application.java
@@ -0,0 +1,13 @@
+package ru.stomprf.discount51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Discount51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Discount51Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/stomprf/discount51/Discount51ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stomprf.discount51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Discount51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
