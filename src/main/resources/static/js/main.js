48577d41d84045bb1bb7352f0a2521f602bc6758
jubeymitsu <jubey.mitsu@gmail.com>
2023-05-26 20:48:46 +0300

    delete with notification

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,9 @@
-function showConfirmationAndDelete(id) {
-    console.log(Id)
+function showConfirmationAndDelete(event) {
+    console.log(event.target)
+    console.log(event.target.id)
+
+    let id = event.target.id.replace("d", "");
+    console.log(id)
     if (confirm('Вы точно хотите удалить этого пользователя?')) {
         window.location.href = `/users/delete/${id}`;
     }
